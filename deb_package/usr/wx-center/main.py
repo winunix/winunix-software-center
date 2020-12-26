66717200e8f9d57ee465c319c6084198d21193c7
ailtonbsj <ailton.ifce@gmail.com>
2020-12-26 02:47:27 -0300

    implemented progressbar

--- deb_package/usr/wx-center/main.py
+++ deb_package/usr/wx-center/main.py
@@ -4,93 +4,107 @@
 import os
 import json
 
-moduleList = []
+module_list = []
 
-def listFolderByFolders(path):
-	os.system('cd '+path+' && echo */ > modules.tmp')
-	with open(path+'modules.tmp', 'r') as tmp:
-		foldStr = tmp.read().replace('\n', '')
-	foldArr = foldStr.split("/ ")
-	os.system('cd '+path+' && rm modules.tmp')
-	foldArr = map(lambda x: x.replace('/',''), foldArr)
-	return foldArr
 
-def listCSVFiles(path):
-	os.system('cd '+path+' && echo *.csv > profiles.tmp')
-	with open(path+'profiles.tmp', 'r') as tmp:
-		fileStr = tmp.read().replace('\n', '')
-	fileArr = fileStr.split(".csv ")
-	fileArr = map(lambda x: x.replace('.csv',''), fileArr)
-	os.system('cd '+path+' && rm profiles.tmp')
-	return fileArr
+def list_folder_by_folders(path):
+    os.system('cd '+path+' && echo */ > modules.tmp')
+    with open(path+'modules.tmp', 'r') as tmp:
+        fold_str = tmp.read().replace('\n', '')
+    fold_arr = fold_str.split("/ ")
+    os.system('cd '+path+' && rm modules.tmp')
+    fold_arr = map(lambda x: x.replace('/', ''), fold_arr)
+    return fold_arr
 
-def generateProfileBox():
-	profiles = listCSVFiles('./profiles/')
-	isChecked = 'TRUE'
-	frag = ''
-	for profile in profiles:
-		frag += isChecked+' "'+profile+'" \\\n'
-		if(isChecked == 'TRUE'):
-			isChecked = 'FALSE'
-	boxProfile = """perfil=$(
+
+def list_csv_files(path):
+    os.system('cd '+path+' && echo *.csv > profiles.tmp')
+    with open(path+'profiles.tmp', 'r') as tmp:
+        file_str = tmp.read().replace('\n', '')
+    file_arr = file_str.split(".csv ")
+    file_arr = map(lambda x: x.replace('.csv', ''), file_arr)
+    os.system('cd '+path+' && rm profiles.tmp')
+    return file_arr
+
+
+def generate_profile_box():
+    profiles = list_csv_files('./profiles/')
+    is_checked = 'TRUE'
+    frag = ''
+    for profile in profiles:
+        frag += is_checked+' "'+profile+'" \\\n'
+        if(is_checked == 'TRUE'):
+            is_checked = 'FALSE'
+    box_profile = """perfil=$(
 	zenity --list --radiolist --title="Perfis" --text="Selecione o perfil" \\
-	--column="" --column="Perfil" --width=200 --height=200 --separator=':' \\
+	--column="" --column="Perfil" --width=300 --height=400 --separator=':' \\
 	"""+frag+') && echo $perfil > selected.tmp'
-	os.system(boxProfile)
+    os.system(box_profile)
 
-def getListProfile():
-	generateProfileBox()
-	with open('selected.tmp', 'r') as tmp:
-		profile = tmp.read().replace('\n', '')
-	os.system('rm selected.tmp')
-	with open('./profiles/'+profile+'.csv') as tmp:
-		selecteds = tmp.read().split('\n')
-	return selecteds
 
-def getListModules():
-	global moduleList
-	path = './modules/'
-	modules = listFolderByFolders(path)
-	modArr = []
-	for module in modules:
-		filename = path+module+'/manifest.json'
-		if os.path.isfile(filename):
-			with open(filename, 'r') as tmp:
-				manifest = tmp.read()
-			maniArr = json.loads(manifest)
-			maniArr['name'] = maniArr['name'].replace(' ', '')
-			modArr.append(maniArr)
-	moduleList = modArr
-	return modArr
+def get_list_profile():
+    generate_profile_box()
+    with open('selected.tmp', 'r') as tmp:
+        profile = tmp.read().replace('\n', '')
+    os.system('rm selected.tmp')
+    with open('./profiles/'+profile+'.csv') as tmp:
+        selecteds = tmp.read().split('\n')
+    return selecteds
 
-def generateAppsBox():
-	selecteds = getListProfile()
-	frag = ''
-	for module in getListModules():
-		isChecked = 'FALSE'
-		for sel in selecteds:
-			if sel == module['name']:
-				isChecked = 'TRUE'
-				break
-		frag += isChecked+' "'+module['name']+'" "'+module['description']+'" \\'
-	boxApps = """softs=$(zenity --list --checklist \\
+
+def get_list_modules():
+    global module_list
+    path = './modules/'
+    modules = list_folder_by_folders(path)
+    mod_arr = []
+    for module in modules:
+        filename = path+module+'/manifest.json'
+        if os.path.isfile(filename):
+            with open(filename, 'r') as tmp:
+                manifest = tmp.read()
+            mani_arr = json.loads(manifest)
+            mani_arr['name'] = mani_arr['name'].replace(' ', '')
+            mod_arr.append(mani_arr)
+    module_list = mod_arr
+    return mod_arr
+
+
+def generate_apps_box():
+    selecteds = get_list_profile()
+    frag = ''
+    for module in get_list_modules():
+        is_checked = 'FALSE'
+        for sel in selecteds:
+            if sel == module['name']:
+                is_checked = 'TRUE'
+                break
+        frag += is_checked+' "'+module['name'] + \
+            '" "'+module['description']+'" \\'
+    box_apps = """softs=$(zenity --list --checklist \\
 	--title="Pacotes a ser instalados" \\
 	--text="lista de pacotes" --column="" --column="Pacote" \\
 	--column="Descricao" --width=700 --height=400 --separator=':' \\
 	"""+frag+"""
 	) 2>/dev/null && echo $softs > apps.tmp"""
-	os.system(boxApps)
+    os.system(box_apps)
 
-generateAppsBox()
+
+def progress(title):
+    return " | zenity --progress --no-cancel --auto-close --text=\"Espere um pouco...\" --title=\"Processando "+title+"\" --width=350"
+
+
+generate_apps_box()
 with open('apps.tmp', 'r') as tmp:
-	modStr = tmp.read().replace('\n', '')
-modArr = modStr.split(":")
+    modStr = tmp.read().replace('\n', '')
+mod_arr = modStr.split(":")
 os.system('rm apps.tmp')
-for mod in moduleList:
-	os.system("./zprogress "+mod['name']+" &")
-	if mod['name'] in modArr:
-		os.system('cd ./modules/'+mod['name']+'/ && ./install')
-	else:
-		os.system('cd ./modules/'+mod['name']+'/ && ./install -u')
-	os.system("sleep 1 && pkill zenity")
-os.system("./zprogress SuperUser &")
+for mod in module_list:
+    if mod['name'] in mod_arr:
+        os.system('cd ./modules/'+mod['name'] +
+                  '/ && ./install'+progress(mod['name']))
+    else:
+        os.system('cd ./modules/'+mod['name'] +
+                  '/ && ./install -u'+progress(mod['name']))
+
+os.system("echo 'apt autoremove -y' >> /tmp/superuser.execute")
+os.system("pkexec /tmp/superuser.execute"+progress('Superusuário'))
